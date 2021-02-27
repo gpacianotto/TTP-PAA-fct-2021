0b42c6ab3cb7821caf04697f4b5ebdd51126e9d8
Guilherme de Aguiar <gpacianotto@gmail.com>
2021-02-27 16:08:47 -0300

    heapSort

--- Master/src/master/Master.java
+++ Master/src/master/Master.java
@@ -236,7 +236,53 @@ public class Master {
             array[i+1] = key;  
         }
         return array;
-    }  
+    }
+    	public static void heapSort(int arr[])
+	{
+		int n = arr.length;
+
+		// Build heap (rearrange array)
+		for (int i = n / 2 - 1; i >= 0; i--)
+			heapify(arr, n, i);
+
+		// One by one extract an element from heap
+		for (int i = n - 1; i > 0; i--) {
+			// Move current root to end
+			int temp = arr[0];
+			arr[0] = arr[i];
+			arr[i] = temp;
+
+			// call max heapify on the reduced heap
+			heapify(arr, i, 0);
+		}
+	}
+
+	// To heapify a subtree rooted with node i which is
+	// an index in arr[]. n is size of heap
+	public static void heapify(int arr[], int n, int i)
+	{
+		int largest = i; // Initialize largest as root
+		int l = 2 * i + 1; // left = 2*i + 1
+		int r = 2 * i + 2; // right = 2*i + 2
+
+		// If left child is larger than root
+		if (l < n && arr[l] > arr[largest])
+			largest = l;
+
+		// If right child is larger than largest so far
+		if (r < n && arr[r] > arr[largest])
+			largest = r;
+
+		// If largest is not root
+		if (largest != i) {
+			int swap = arr[i];
+			arr[i] = arr[largest];
+			arr[largest] = swap;
+
+			// Recursively heapify the affected sub-tree
+			heapify(arr, n, largest);
+		}
+	}
        private static void autoCountRandom(int limite, String algorythm, int numberOfTests){
            
            for(int i = 0; i < numberOfTests; i++)
@@ -308,18 +354,25 @@ public class Master {
                         segundos = (double)milisec/1000;
                         System.out.println("Milisegundos: "+milisec+" Segundos: "+segundos);
                         break;
+                    case "heapSort":
+                        start = System.currentTimeMillis();
+                        heapSort(arranjo);
+                        milisec = System.currentTimeMillis() - start;
+                        segundos = (double)milisec/1000;
+                        System.out.println("Milisegundos: "+milisec+" Segundos: "+segundos);
+                        break;
                 }
            }
            
        }
     
     public static void main(String[] args) {
-        final int limite = 900000;
+        final int limite = 5000000;
         
         //limite de quickSort maximo é 7500, se não da stack overflow
         //limite de quickSort2 maximo é 15400, se não da stack overflow
         
-        autoCountRandom(limite, "insertionSort", 10);
+        autoCountRandom(limite, "heapSort", 10);
         
         //valor maximo de int
         System.out.println(Integer.MAX_VALUE);
